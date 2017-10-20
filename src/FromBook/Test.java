c41a18a3f0355edd4d1451e807e40d906ff61833
omoskaliuk86 <sasharohoziv08041986>
2017-10-20 16:54:01 +0300

    UserService

--- src/FromBook/Test.java
+++ src/FromBook/Test.java
@@ -5,36 +5,67 @@ import java.awt.*;
 
 
 
-public class Test extends JFrame{
+public class Test{
     public Test() {
-        setTitle("Test");
-        setLayout(new FlowLayout());
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        JFrame jfrm = new JFrame("Test");
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout());
+        jfrm.add(mainPanel);
+        jfrm.setSize(400, 300);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setVisible(true);
 /*
         JTabbedPane jtb = new JTabbedPane();
         jtb.addTab("Cities", new CitiesPanel());
         jtb.addTab("Colors", new ColorsPanel());
         jtb.addTab("Flavors", new FlavorsPanel());
 
-        add(jtb);
+        jfrm.add(jtb);
 */
 /*
-        JPanel jp = new JPanel();
-        jp.setLayout(new GridLayout(20,20));
+        JPanel jp1 = new JPanel();
+        JLabel jl1 = new JLabel("First Name");
+        JLabel jl2 = new JLabel("Last Name");
+        jp1.add(jl1);
+        jp1.add(jl2);
+        jfrm.add(jp1);
 
+
+        JPanel jp2 = new JPanel();
+        JButton jb1 = new JButton("Add photo");
+        JButton jb2 = new JButton("Friends");
+        jp2.add(jb1);
+        jp2.add(jb2);
+        jfrm.add(jp2);
+
+
+
+        JPanel jp3 = new JPanel();
+        jp3.setLayout(new GridLayout(20,5));
         int b = 0;
         for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                jp.add(new JButton("Button " + b));
+            for (int j = 0; j < 5; j++) {
+                jp3.add(new JButton("Button " + b));
                 ++b;
             }
         }
-
-        JScrollPane jsp = new JScrollPane(jp);
-        add(jsp, BorderLayout.CENTER);
+        JScrollPane jsp = new JScrollPane(jp3);
+        jfrm.add(jsp,BorderLayout.CENTER);
 */
+
+        JPanel jp3 = new JPanel();
+        jp3.setLayout(new GridLayout(20,5));
+        int b = 0;
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 5; j++) {
+                jp3.add(new JButton("Button " + b));
+                ++b;
+            }
+        }
+        JScrollPane jsp = new JScrollPane(jp3);
+        jfrm.add(jsp,BorderLayout.CENTER);
+
+
 
     }
 }
